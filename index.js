74682d1d1011949cedbe2094945734ce20f89507
HdGampro <38081585+HdGampro@users.noreply.github.com>
2018-04-04 19:39:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,78 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const PREFIX = "/"
+bot.login(process.env.TOKEN);
 
 bot.on('ready', function() {
     console.log("I'm ready !");
 });
 
-bot.on("message", message => {
-	if(message.content.startsWith(PREFIX + "heloo")) {
-		message.channel.send("Salut")
-	}
-})
+// Configs
+const PREFIX = "/";
 
-bot.login(process.env.TOKEN) // token
+bot.on("message", message => {
+
+
+    // Commande /hello
+    if(command === "hello"){
+        message.reply('Salut !');
+    }
+
+     if(command === "help"){
+        message.reply("page d'aide affichée avec sucès");
+    }
+
+    if(message.content.startsWith(PREFIX + `info`)) {
+        var memberavatar = message.author.avatarURL
+        var membername = message.author.username
+           var mentionned = message.mentions.users.first();
+          var getvalueof;
+          if(mentionned){
+              var getvalueof = mentionned;
+          } else {
+              var getvalueof = message.author;
+          }
+      
+          if(getvalueof.bot == true){
+              var checkbot = "L'utilisateur est un bot";
+          } else {
+              var checkbot = "N'est pas un bot";
+          }
+          if(getvalueof.presence.status == 'online'){
+            var status = "En ligne";
+          }else {
+            var status = "Hors ligne";
+          }
+      
+        message.channel.sendMessage({
+            embed: {
+              type: 'rich',
+              description: '',
+              fields: [{
+                name: 'Pseudonyme :',
+                value: getvalueof.username,
+                inline: true
+              }, {
+                name: 'ID :',
+                value: getvalueof.id,
+                inline: true
+              },{
+                name: 'Discriminateur :',
+                value: getvalueof.discriminator,
+                inline: true
+      },{
+                name: 'Status :',
+                value: status,
+                inline: true
+      },{
+                name: 'Bot :',
+                value: checkbot,
+                inline: true
+      }],
+            }
+      });
+      
+      }
+
+
+});
