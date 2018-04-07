8594bfa1776396ee9bcad8ec39d6cc08a680296d
HdGampro <38081585+HdGampro@users.noreply.github.com>
2018-04-08 00:11:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,18 +18,15 @@ bot.on("message", (message) => {
     if(command === "hello"){
         message.reply('Salut !');
      }else if(command === "help"){
-        message.reply("page d'aide affichée avec sucès !
+        message.reply("page d'aide affichée avec succès !
                       \n /help; affiche les commande du bot
                       \n /hello pour interagir avec le bot
                       \n /test pour tester si le bot est online
-                      \n /breuh bas pour recevoir un 'breuh' du bot ^^);
+                      \n /breuh bas pour recevoir un 'breuh' du bot ^^");
      }else if(command === "test"){
          message.reply("test de connection avec le bot reussi !")
      }
-
 });
-
 bot.on("guildMemberAdd", (member) => {
     member.guild.channels.find("name", "bienvenue").send(`bienvenue ${member} n'hésite pas à invité tes amis pour obtenir le grade @V.I.P`)
-
 });
