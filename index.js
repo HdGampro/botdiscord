663a61dbe8f308cbd28fcfc1dba12698d2fb091a
HdGampro <38081585+HdGampro@users.noreply.github.com>
2018-04-07 23:31:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
-bot.login(process.env.TOKEN);
-
+bot.login('NDMyMjYwOTE0MjQzMzA1NDgy.DaquNA.50Go60_aLkhcv_XnHqrZEcDSVEs');
 bot.on('ready', function() {
     console.log("I'm ready !");
 });
 
-// Configs
-const PREFIX = "/";
+bot.on("message", (message) => {
 
-bot.on("message", message => {
+    // Configs
+    const PREFIX = ("/");
+    if (!message.content.startsWith(PREFIX) || message.guild === null) return;
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
 
     // Commande /hello
-    if(message.content.startsWith(PREFIX + "hello")) {
-        message.reply("Salut")
-    }
-    
-    if(message.content.startsWith(PREFIX + "help")) {
-       message.reply("Page d'aide affichée avec succès !") 
-    }
+    if(command === "hello"){
+        message.reply('Salut !');
+     }else if(command === "help"){
+        message.reply("page d'aide affichée avec sucès");
+     }else if(command === "test"){
+         message.reply("test de connection avec le bot reussi !")
+     }
+
+});
+
+bot.on("guildMemberAdd", (member) => {
+    member.guild.channels.find("name", "bienvenue").send(`bienvenue ${member} n'hésite pas à invité tes amis pour obtenir le grade @V.I.P`)
 
 });
