aae393b4f834130da156191d7acf3e59878ef75a
HdGampro <38081585+HdGampro@users.noreply.github.com>
2018-04-08 12:26:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ bot.on("guildMemberAdd", (member) => {
     member.guild.channels.find("name", "bienvenue").send(`bienvenue ${member} n'hésite pas à invité tes amis pour obtenir le grade @V.I.P`)
 });
 bot.on("guildaMemberAdd", member => {
-    var role = member.guild.role.find('name', membre);
+    var role = member.guild.role.find('name', "membre");
     member.addRole(role)
 });
